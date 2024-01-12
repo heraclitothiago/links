3ed5c2d6e67ef2489a3f8fd7c4db8f104523f6bd
Thiago <heraclitothiago@hotmail.com>
2024-01-11 22:47:09 -0300

    página de links finalizada

---
+++ src/components/links.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TbFaceIdError } from "react-icons/tb";
+
+export default function Links({ site, url, icon: Icon }) {
+    return (
+        <a className='flex flex-row items-center gap-3
+         justify-center w-9/12 rounded-3xl border-white border-2 mb-2 py-1.5
+        bg-transparent text-white font-semibold
+        hover:bg-white hover:text-black hover:transition hover:duration-500 ease-in-out'
+            href={url || "#"} target='_blank'
+            rel='noopener noreferrer'>
+            {Icon ? <Icon /> : <TbFaceIdError />}
+            {site || "site"}
+        </a>
+    )
+}
