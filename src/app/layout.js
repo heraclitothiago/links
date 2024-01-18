5ee2366723035f66c2249376668c78d1cbabc0d9
Thiago <heraclitothiago@hotmail.com>
2024-01-18 17:10:01 -0300

    google analytics refatorado, implementação bem sucedida

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Head from "next/head"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,18 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <Head>
-        <Script id='analytics' strategy='beforeInteractive' async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}></Script>
-        <Script id='gtag'>{`
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${process.env.GTAG_ID}');
-        `}</Script>
-      </Head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId={process.env.GTAG_ID} />
     </html>
   )
 }
