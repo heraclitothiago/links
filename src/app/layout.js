00c74f9f804a529e2af53930c1acd4e895706750
Thiago Castro <heraclitothiago@hotmail.com>
2024-01-23 15:06:22 -0300

    adicionado analytics da vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>{children}</body>
-      {/* <GoogleAnalytics gaId={process.env.GTAG_ID} /> */}
+      <Analytics />
     </html>
   )
 }
